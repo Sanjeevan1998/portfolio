05c2d33ecdcbfc8df51cdf229f45d103ccb8ddb8
Sanjeevan <sanjeevan.adhikari@gmail.com>
2025-05-05 06:02:44 -0400

    Build website to /docs folder and configure for Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // --- CHANGE THIS LINE ---
-  base: '/portfolio/', // Set base path for username.github.io/repository-name/ URL
-  // --- END CHANGE ---
+  // Base path for username.github.io/repository-name/ URL
+  base: '/portfolio/',
+  // --- ADD THIS BUILD SECTION ---
+  build: {
+    outDir: 'docs', // Output directory changed from 'dist' to 'docs'
+  },
+  // --- END BUILD SECTION ---
 });
